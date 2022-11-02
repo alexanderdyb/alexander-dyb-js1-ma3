8a4554a0267148f91a1c09ae9b16ed4d41dd6c77
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2022-11-02 21:26:47 +0100

    Update

---
+++ js/question-1.js
@@ -0,0 +1,10 @@
+// function getRemainder(a,b) {
+//     return a % b;
+//     }
+
+// Arrow function:
+
+const getRemainder = (a, b) => a % b;
+
+const result = getRemainder(5, 2);
+console.log(result);
