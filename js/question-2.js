76daf058527508a5343a71403e45c23baffaadbe
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2022-11-03 20:56:45 +0100

    Update

---
+++ js/question-2.js
@@ -0,0 +1,31 @@
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=edee90318ac5464caab996801c1cc443";
+
+const moviesContainer = document.querySelector(".movies");
+
+async function getData() {
+  const response = await fetch(url);
+
+  const results = await response.json();
+
+  const allData = results.results;
+
+  moviesContainer.innerHTML = "";
+
+  for (let i = 0; i < allData.length; i++) {
+    if (i === 7) {
+      break;
+    }
+    const name = allData[i].name;
+    const rating = allData[i].rating;
+    const numberTags = allData[i].tags.length;
+
+    moviesContainer.innerHTML += `<div class="movieList">
+    Name: ${name} <br>
+    Rating: ${rating} <br>
+    Number of tags: ${numberTags}                                        
+    </div>`;
+  }
+}
+
+getData();
