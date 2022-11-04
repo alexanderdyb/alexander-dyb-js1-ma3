6e040870397a1e499693ba3a1d551c19d8bb97d0
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2022-11-04 09:07:51 +0100

    Update

---
+++ js/components/displayError.js
@@ -0,0 +1,3 @@
+function displayError(message) {
+  return `<div class="error"><p>${message}</p></div>`;
+}
--- js/question-2.js
+++ js/question-2.js
@@ -1,30 +1,35 @@
-const url =
-  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=edee90318ac5464caab996801c1cc443";
+const apiKey = "edee90318ac5464caab996801c1cc443";
+
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 
 const moviesContainer = document.querySelector(".movies");
 
 async function getData() {
-  const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-  const results = await response.json();
+    const results = await response.json();
 
-  const allData = results.results;
+    const allData = results.results;
 
-  moviesContainer.innerHTML = "";
+    moviesContainer.innerHTML = "";
 
-  for (let i = 0; i < allData.length; i++) {
-    if (i === 7) {
-      break;
+    for (let i = 0; i < allData.length; i++) {
+      if (i === 7) {
+        break;
+      }
+      const name = allData[i].name;
+      const rating = allData[i].rating;
+      const numberTags = allData[i].tags.length;
+
+      moviesContainer.innerHTML += `<div class="movieList">
+        <p><span>Name:</span> ${name}</p> 
+        <p><span>Rating:</span> ${rating}</p>
+        <p><span>Number of tags:</span> ${numberTags}</p>                                        
+        </div>`;
     }
-    const name = allData[i].name;
-    const rating = allData[i].rating;
-    const numberTags = allData[i].tags.length;
-
-    moviesContainer.innerHTML += `<div class="movieList">
-    Name: ${name} <br>
-    Rating: ${rating} <br>
-    Number of tags: ${numberTags}                                        
-    </div>`;
+  } catch (error) {
+    moviesContainer.innerHTML = displayError("An error occured");
   }
 }
 
