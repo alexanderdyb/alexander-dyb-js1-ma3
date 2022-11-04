582c112a552ee757b9092cc9d31a16ecaa0e0b07
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2022-11-04 09:19:23 +0100

    Update

--- js/question-2.js
+++ js/question-2.js
@@ -2,7 +2,7 @@ const apiKey = "edee90318ac5464caab996801c1cc443";
 
 const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 
-const moviesContainer = document.querySelector(".movies");
+const gamesContainer = document.querySelector(".games");
 
 async function getData() {
   try {
@@ -12,7 +12,7 @@ async function getData() {
 
     const allData = results.results;
 
-    moviesContainer.innerHTML = "";
+    gamesContainer.innerHTML = "";
 
     for (let i = 0; i < allData.length; i++) {
       if (i === 7) {
@@ -22,7 +22,7 @@ async function getData() {
       const rating = allData[i].rating;
       const numberTags = allData[i].tags.length;
 
-      moviesContainer.innerHTML += `<div class="movieList">
+      gamesContainer.innerHTML += `<div class="gameList">
         <p><span>Name:</span> ${name}</p> 
         <p><span>Rating:</span> ${rating}</p>
         <p><span>Number of tags:</span> ${numberTags}</p>                                        
